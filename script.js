9a7851921e0e3c3631e41e49bf493205c3ca2601
dtcpass-delhi-gov-inview-epass-no <166311460+dtcpass-delhi-gov-inview-epass-no@users.noreply.github.com>
2025-05-27 07:49:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('busForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  window.location.href = 'https://dtcbuspass-delhi-gov-in-view-epassno.github.io/7502012543261/';
+});
